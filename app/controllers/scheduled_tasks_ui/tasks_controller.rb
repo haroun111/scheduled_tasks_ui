87d256561d5e77a99d493a717b25e367c7ccecbf
haroun111 <haroundhim97@gmail.com>
2025-07-14 16:01:12 +0100

    fix task controller

--- app/controllers/scheduled_tasks_ui/tasks_controller.rb
+++ app/controllers/scheduled_tasks_ui/tasks_controller.rb
@@ -3,8 +3,22 @@ module ScheduledTasksUi
     before_action :set_refresh, only: [:index]
 
     def index
-      @available_tasks = ScheduledTasksUi::TaskDataIndex.available_tasks.group_by(&:category)
+      puts "Loaded task constants:"
+      puts ScheduledTasksUi::Task.constants.inspect
+    
+      tasks = ScheduledTasksUi::Task.constants.map do |const|
+        task_class = ScheduledTasksUi::Task.const_get(const)
+        OpenStruct.new(
+          name: task_class.name,
+          category: task_class.respond_to?(:category) ? task_class.category : "Default",
+          description: task_class.respond_to?(:description) ? task_class.description : "",
+          status: :new
+        )
+      end
+    
+      @available_tasks = { new: tasks, active: [], completed: [] }
     end
+    
 
     def show
       @task = ScheduledTasksUi::TaskDataShow.prepare(
