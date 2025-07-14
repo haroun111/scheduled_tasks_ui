a9ec04efe41d55f2d0f44cd86e40d3bc7c36cf40
haroun111 <haroundhim97@gmail.com>
2025-07-14 14:50:59 +0100

    fix engine class

--- lib/scheduled_tasks_ui/engine.rb
+++ lib/scheduled_tasks_ui/engine.rb
@@ -3,9 +3,12 @@ module ScheduledTasksUi
     isolate_namespace ScheduledTasksUi
 
     initializer 'scheduled_tasks_ui.load_tasks' do
-      # Permet aux apps d'ajouter leurs propres tâches dans app/tasks/scheduled_tasks_ui/tasks
       tasks_path = Rails.root.join('app', 'tasks', 'scheduled_tasks_ui', 'tasks')
-      $LOAD_PATH.unshift(tasks_path) if Dir.exist?(tasks_path)
+      if Dir.exist?(tasks_path)
+        Dir[tasks_path.join('*.rb')].sort.each do |file|
+          require file
+        end
+      end
     end
   end
 end
