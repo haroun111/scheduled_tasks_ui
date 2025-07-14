2cbc03f4dea14e183f539cc204de746dd0ad8e49
haroun111 <haroundhim97@gmail.com>
2025-07-14 14:24:41 +0100

    change name class

--- lib/scheduled_tasks_ui/task_data_index.rb
+++ lib/scheduled_tasks_ui/task_data_index.rb
@@ -1,8 +1,8 @@
 module ScheduledTasksUi
   class TaskDataIndex
     def self.available_tasks
-      ScheduledTasksUi::Tasks.constants.map do |const_name|
-        task_class = ScheduledTasksUi::Tasks.const_get(const_name)
+      ScheduledTasksUi::Task.constants.map do |const_name|
+        task_class = ScheduledTasksUi::Task.const_get(const_name)
         OpenStruct.new(
           name: task_class.name,
           category: task_class.respond_to?(:category) ? task_class.category : "Default",
--- lib/scheduled_tasks_ui/task_data_show.rb
+++ lib/scheduled_tasks_ui/task_data_show.rb
@@ -1,7 +1,7 @@
 module ScheduledTasksUi
   class TaskDataShow
     def self.prepare(task_name, runs_cursor: nil, arguments: {})
-      task_class = ScheduledTasksUi::Tasks.const_get(task_name)
+      task_class = ScheduledTasksUi::Task.const_get(task_name)
       runs = ScheduledTasksUi::Run.where(task_name: task_name).order(created_at: :desc).limit(10)
       OpenStruct.new(task: task_class, runs: runs, arguments: arguments)
     end
