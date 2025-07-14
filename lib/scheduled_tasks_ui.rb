09e8f297a6c006edfc0839b6bcbd5c185cda556d
haroun111 <haroundhim97@gmail.com>
2025-07-14 14:12:23 +0100

    add require_relative

--- lib/scheduled_tasks_ui.rb
+++ lib/scheduled_tasks_ui.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
-
 require_relative "scheduled_tasks_ui/version"
-require "scheduled_tasks_ui/engine"
-require "scheduled_tasks_ui/task"
-require "scheduled_tasks_ui/task_data_index"
+require_relative "scheduled_tasks_ui/engine"
+require_relative "scheduled_tasks_ui/task"
+require_relative "scheduled_tasks_ui/task_data_index"
 module ScheduledTasksUi
   class Error < StandardError; end
   # Your code goes here...
